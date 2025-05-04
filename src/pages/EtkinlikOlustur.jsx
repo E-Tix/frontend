29a3c9ed2ad7f9ce9ea7f8deaf3a5ab0c4dc5930
MeryemEceAYYILDIZ <meryem_ece06@hotmail.com>
2025-05-05 01:49:28 +0300

    Etkinlik ekleme tamamlandı

--- src/pages/EtkinlikOlustur.jsx
+++ src/pages/EtkinlikOlustur.jsx
@@ -3,60 +3,92 @@ import axios from 'axios';
 import { useAuth } from "../context/AuthContext";
 
 const AddEvent = () => {
-  const { user } = useAuth();
-  const [etkinlikAdi, setEtkinlikAdi] = useState('');
-  const [etkinlikAciklamasi, setEtkinlikAciklamasi] = useState('');
-  const [yasSiniri, setYasSiniri] = useState('');
-  const [biletFiyati, setBiletFiyati] = useState('');
-  const [etkinlikSuresi, setEtkinlikSuresi] = useState('');
-  const [kapakFotografi, setKapakFotografi] = useState('');
-  const [seanslar, setSeanslar] = useState([]);
-  const [yeniSeans, setYeniSeans] = useState('');
-  const [sehirler, setSehirler] = useState([]);
-  const [salonlar, setSalonlar] = useState([]);
-  const [etkinlikTurleri, setEtkinlikTurleri] = useState([]);
-  const [secilenSehir, setSecilenSehir] = useState('');
-  const [secilenSalon, setSecilenSalon] = useState('');
-  const [secilenTur, setSecilenTur] = useState('');
+    const { user } = useAuth();
+    const [etkinlikAdi, setEtkinlikAdi] = useState('');
+    const [etkinlikAciklamasi, setEtkinlikAciklamasi] = useState('');
+    const [yasSiniri, setYasSiniri] = useState('');
+    const [biletFiyati, setBiletFiyati] = useState('');
+    const [etkinlikSuresi, setEtkinlikSuresi] = useState('');
+    const [kapakFotografi, setKapakFotografi] = useState('');
+    const [seanslar, setSeanslar] = useState([]);
+    const [yeniSeans, setYeniSeans] = useState('');
+    const [sehirler, setSehirler] = useState([]);
+    const [salonlar, setSalonlar] = useState([]);
+    const [etkinlikTurleri, setEtkinlikTurleri] = useState([]);
+    const [secilenSehir, setSecilenSehir] = useState('');
+    const [secilenSalon, setSecilenSalon] = useState('');
+    const [secilenTur, setSecilenTur] = useState('');
 
-  useEffect(() => {
-    axios.get('http://localhost:8080/sehir/sehirler')
-        .then(res => setSehirler(res.data))
-        .catch((err) => {
-        console.error("Sehirleri alma başarısız:", err);
-    });
-    axios.get('http://localhost:8080/veri/salonlar').then(res => setSalonlar(res.data))
-    .catch((err) => {
-        console.error("Salonları alma başarısız:", err);
-        alert("Salonları alma başarısız:");
-    });
-    axios.get('http://localhost:8080/veri/etkinlik-turleri').then(res => setEtkinlikTurleri(res.data))
-    .catch((err) => {
-        console.error("Turleri alma başarısız:", err);
-        alert("Türleri alma başarısız:");
-    });
-  }, []);
+    useEffect(() => {
+        axios.get('http://localhost:8080/sehir/sehirler')
+            .then(res => setSehirler(res.data))
+            .catch((err) => {
+                console.error("Sehirleri alma başarısız:", err);
+            });
+        axios.get('http://localhost:8080/veri/etkinlik-turleri').then(res => setEtkinlikTurleri(res.data))
+            .catch((err) => {
+                console.error("Turleri alma başarısız:", err);
+                alert("Türleri alma başarısız:");
+            });
+    }, []);
 
-  const handleKapakYukle = async (e) => {
-    const file = e.target.files[0];
-    const formData = new FormData();
-    formData.append('file', file);
-    try {
-      const res = await axios.post('http://localhost:8080/etkinlikler/kapak-foto${eventId}', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' }
-      });
-      setKapakFotografi(res.data);   // Fotoğraf ile ilişkilendirilen veri
-    } catch (err) {
-      console.error('Kapak fotoğrafı yüklenemedi', err);
-    }
-  };
+    useEffect(() => {
+        console.log("secilenSehir: ", secilenSehir);
+        if (secilenSehir) {
+        console.log("user.token", user.token);
+        const sehirObje = sehirler.find(s => s.plakaKodu === parseInt(secilenSehir));
+        axios.post('http://localhost:8080/organizatorMainPage/getSalonsForSehir', sehirObje,
+          {headers: {
+            Authorization: `Bearer ${user.token}`,
+          }
+        }
+        )
+            .then(res => {
+                console.log("Salonlar:", res.data);
+                setSalonlar(res.data)})
+            .catch((err) => {
+                console.error("Salonları alma başarısız:", err);
+                alert("Salonları alma başarısız:");
+            });
+        } else {
+            setSalonlar([]);
+            setSecilenSalon('');
+        }
+    }, [secilenSehir, user.token])
 
-  const handleSeansEkle = () => {
-    if (yeniSeans) {
-      setSeanslar([...seanslar, { tarih: yeniSeans }]);
-      setYeniSeans('');
-    }
-  };
+    const handleKapakYukle = async (e) => {
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('file', file);
+        try {
+            const res = await axios.post('http://localhost:8080/etkinlikler/kapak-foto/${eventId}', formData, {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            });
+            setKapakFotografi(res.data);   // Fotoğraf ile ilişkilendirilen veri
+        } catch (err) {
+            console.error('Kapak fotoğrafı yüklenemedi', err);
+        }
+    };
+
+    const handleSeansEkle = () => {
+        if (yeniSeans && etkinlikSuresi) {
+          const seansTarih = new Date(yeniSeans);
+          const bitisTarih = new Date(seansTarih.getTime() + etkinlikSuresi * 60000);
+          setSeanslar([...seanslar, {
+            tarih: seansTarih.toISOString(),
+            bitisTarihi: bitisTarih.toISOString()
+          }]);
+          setYeniSeans('');
+        }
+      };
+
+    const handleSalonSecimi = (salonID) => {
+        setSecilenSalon(prev =>
+          prev.includes(salonID)
+            ? prev.filter(id => id !== salonID)
+            : [...prev, salonID]
+        );
+      };
 
   const handleSubmit = async () => {
     const dto = {
@@ -64,12 +96,12 @@ const AddEvent = () => {
       etkinlikAciklamasi,
       yasSiniri: parseInt(yasSiniri),
       biletFiyati: parseFloat(biletFiyati),
-      etkinlikSuresi,
+      etkinlikSuresi: parseInt(etkinlikSuresi),
       kapakFotografi,
       sehir: sehirler.find(s => s.plakaKodu === parseInt(secilenSehir)),
       salon: salonlar.find(s => s.salonID === parseInt(secilenSalon)),
       etkinlikTur: etkinlikTurleri.find(t => t.etkinlikTurID === parseInt(secilenTur)),
-      seansEkleDtoList: seanslar.map(s => ({ tarih: s.tarih }))
+      seansEkleDtoList: seanslar.map(s => ({ tarih: s.tarih, bitisTarihi: s.bitisTarihi }))
     };
 
     try {
@@ -141,7 +173,7 @@ const AddEvent = () => {
             <div>
                 <label className="">Etkinlik Süresi</label>
                 <input
-                    type="datetime-local"
+                    type="number"
                     placeholder="Etkinlik Süresi"
                     className="input"
                     value={etkinlikSuresi}
@@ -166,28 +198,43 @@ const AddEvent = () => {
             </div>
 
             <div>
-                <select value={secilenSalon} onChange={e => setSecilenSalon(e.target.value)}>
-                    <option>Salon Seçin</option>
-                    {salonlar.map(salon => <option key={salon.salonID} value={salon.salonID}>{salon.salonAdi}</option>)}
-                </select>
-            </div>
-
-            <div>
                 <select value={secilenTur} onChange={e => setSecilenTur(e.target.value)}>
                     <option>Tür Seçin</option>
                     {etkinlikTurleri.map(tur => <option key={tur.etkinlikTurID} value={tur.etkinlikTurID}>{tur.etkinlikTurAdi}</option>)}
                 </select>
             </div>
 
-            <div className="my-4">
-                <input type="datetime-local" value={yeniSeans} onChange={e => setYeniSeans(e.target.value)} />
-                <button onClick={handleSeansEkle}>Seans Ekle</button>
-                <ul>
-                    {seanslar.map((s, index) => (
-                        <li key={index}>{s.tarih}</li>
-                    ))}
-                </ul>
-            </div>
+            {salonlar.length > 0 && (
+                    <>
+                      <label>Salon</label>
+                      <select value={secilenSalon} onChange={e => setSecilenSalon(e.target.value)} required>
+                        <option value="">Salon seçiniz</option>
+                        {salonlar.map((salon) => (
+                          <option key={salon.salonID} value={salon.salonID}>
+                            {salon.salonAdi} - {salon.salonAdresi}
+                          </option>
+                        ))}
+                      </select>
+                    </>
+                  )}
+
+                  {secilenSalon && (
+                    <>
+                      <label>Yeni Seans Ekle (YYYY-MM-DDTHH:mm)</label>
+                      <input
+                        type="datetime-local"
+                        value={yeniSeans}
+                        onChange={e => setYeniSeans(e.target.value)}
+                      />
+                      <button type="button" onClick={handleSeansEkle}>Seans Ekle</button>
+
+                      <ul>
+                        {seanslar.map((seans, idx) => (
+                          <li key={idx}>{seans.tarih}</li>
+                        ))}
+                      </ul>
+                    </>
+                  )}
 
             <div>
                 <button
